84ac19cb773f06cecaa144b3b2bda4c28f8f8304
Matheus <matheus.herique31@hotmail.com>
2019-12-05 15:15:20 -0300

    RepositoryImpl, service e serviceImpl.

---
+++ src/main/java/repositorio/impl/ReservaRepositoryImpl.java
@@ -0,0 +1,42 @@
+package repositorio.impl;
+
+import java.util.List;
+
+import domain.Reserva;
+import repositorio.ReservaRepository;
+
+public class ReservaRepositoryImpl implements ReservaRepository{
+
+	@Override
+	public boolean inserirReserva(Reserva reserva) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean alterarReserva(Reserva reserva) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean excluirReserva(Reserva reserva) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Reserva> listarReserva() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Reserva getReserva(int id_reserva) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
