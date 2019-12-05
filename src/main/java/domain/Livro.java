5c6b10c161352091909e80909685bb3646aa446c
BrunoOliveiraHHH <bruno.d.l.oliveira@outlook.com>
2019-12-05 14:10:12 -0300

    LIVRO MODEL

--- src/main/java/domain/Livro.java
+++ src/main/java/domain/Livro.java
@@ -2,54 +2,55 @@ package domain;
 
 public class Livro {
 
-	private int idLivro;
+	private int id_livro;
 	private String nome;
 	private String autor;
 	private String isbn;
 	private String genero;
-	
-	
-	public Livro () {
-		
+
+	public Livro() {
+
 	}
-	
-	public int getIdLivro() {
-		return idLivro;
+
+	public int getId_livro() {
+		return id_livro;
 	}
-	public void setIdLivro(int idLivro) {
-		this.idLivro = idLivro;
+
+	public void setId_livro(int id_livro) {
+		this.id_livro = id_livro;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getAutor() {
 		return autor;
 	}
+
 	public void setAutor(String autor) {
 		this.autor = autor;
 	}
+
 	public String getIsbn() {
 		return isbn;
 	}
+
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
+
 	public String getGenero() {
 		return genero;
 	}
+
 	public void setGenero(String genero) {
 		this.genero = genero;
-		
-		
-		
+
 	}
-	
-	
-	
-	
-	
 
 }
