d2d9ec258ea85199df29450adb902ead503e7e10
BrunoOliveiraHHH <bruno.d.l.oliveira@outlook.com>
2019-12-05 13:50:21 -0300

    remoçao classes modelos

--- src/main/java/repositories/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import domain.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-
-}
